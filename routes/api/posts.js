d87a3396719db1893c7449c21c8dab97214dde04
EulerSolutionsMatt <47004936+EulerSolutionsMatt@users.noreply.github.com>
2021-02-04 21:06:05 +0000

    Add get all post, get post by id & delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Post = require('../../models/Post');
 const User = require('../../models/User');
+const mongoose = require('mongoose');
 
 // ADD POST ROUTE
 
@@ -39,5 +40,72 @@ router.post(
     }
   }
 );
+
+// @route GET api/posts
+// @desc Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    posts = await Post.find().sort('-date');
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+  let post_id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(post_id)) {
+    return res.status(400).send('Post not found');
+  }
+
+  try {
+    post = await Post.findById(post_id);
+    if (post.isEmpty) {
+      return res.status(400).send('Post not found');
+    }
+    return res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route DELETE api/posts/:id
+// @desc Delete post by id
+// @access Private
+router.delete('/:id', auth, async (req, res) => {
+  let post_id = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(post_id)) {
+    return res.status(400).send('Post not found');
+  }
+
+  try {
+    post = await Post.findById(post_id);
+    if (!post) {
+      return res.status(400).send('Post not found');
+    }
+
+    if (post.user != res.user.id) {
+      if (post.isEmpty) {
+        return res
+          .status(400)
+          .send('You do not have permission to delete this post.');
+      }
+    }
+
+    post = await Post.findByIdAndRemove(post_id);
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
